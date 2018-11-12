b789559db444c27247e3e06fc0418fb187bda792
Erwan IQUEL <erwan.iquel@gmail.com>
2018-11-12 02:24:29 +0100

    Fix duplication Annotation Injectable

--- taa-gli/src/app/employee.service.ts
+++ taa-gli/src/app/employee.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { tap, catchError, map } from 'rxjs/operators';
